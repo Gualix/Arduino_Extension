98976ee794e080f10a80a3d45c2dc935d2fd4c37
gualix <gualixgo@gmail.com>
2018-12-17 20:32:50 -0600

    CD4094, mejoras en documentación

--- Extension_Code/Extension_Code.ino
+++ Extension_Code/Extension_Code.ino
@@ -1,31 +1,77 @@
 
-int STR = 10;
-int DATA = 11;
-int CLK = 12;
+/*<------------------------------ - Arduino_Extension.h - -----------------------------
+ 
+  Librería que permite expandir la candidad de pines de entrada y salida que se disponen 
+  en el Arduino, utiliza CI CD4094 y CD4021. Esta versión solo está habilitada para dos
+  CD4094 y dos CD4021, por lo tanto 16 nuevos pines de entrada y 16 nuevos de salida.
 
-int a = 0x00; // firts byte  00000000
-int b = 0x00; // second byte 00000000
+  *Los dos CD4094 brindan 16 pines de salida nuevos, que van del 0 al 15.
+     Ejemplo:
+       newOut(3, HIGH); //Pone el pin Q4 del primer CD4094 en <<HIGH>> 
 
-void setup() {
-  //set pins to output because they are addressed in the main loop
+  Creado por: German A. Ureña Araya, 17 diciembre 2018
+  Liberado para el dominio público.
+  
+---------------------------------------------------------------- Versión 1 --------->*/
+
+
+int STR = 10;       // Pin 10 del Arduino conectado a los pines <<STROBE>> de los CD4094
+int DATA_OUT = 11;  // Pin 11 del Arduino conectado al pin <<DATA>> del primer CD4094
+int CLK = 12;       // Pin 12 del Arduino conectado a los pines de <<CLOCK>> de ambos integrados
+
+int a = 0x00;       // Byte de datos a
+int b = 0x00;       // Byte de datos b
+
+void setup(){
+
+  //-------------> Se declaran los pines de salida
   pinMode(STR, OUTPUT);
   pinMode(CLK, OUTPUT);
-  pinMode(DATA, OUTPUT);
-}
-
-void Output(int, int);
-void Output_Byte(int, int);
-
-void loop() {
-
-  Output(1, HIGH);
-  delay(100);
-  Output(1, LOW);
-  delay(100);
+  pinMode(DATA_OUT, OUTPUT);
   
 }
 
-void Output(int pin,int std){
+/*----------------> Función: newOut
+ *
+ *  ->Establece el estado lógico en algún de los pines de los CD4094,  
+ *    según sea recibido en los parámetros.
+ *  ->Recibe como primer parámetro el pin al que se le coloca el estado 
+ *    que se establece en el segundo.
+ * 
+ *-------------------------------------------------------------------->*/
+
+void newOut(int, int);
+
+/*----------------> Función: outByte
+ *
+ *  ->Envía dos bytes a los CD4094, que establecen el estado de las salidas de los pines
+ *    en ambos circuitos integrados.
+ *  ->Recibe como parámetros dos bytes (a, b).
+ * 
+ *-------------------------------------------------------------------->*/
+ 
+void outByte(int, int);
+
+int tmp = 100;
+
+void loop() {
+
+  for(int a = 0; a <= 7; a++){
+    newOut(a, HIGH);
+    for(int d = 8; d <= 15; d++){
+      newOut(d, HIGH);
+      delay(20);
+      newOut(d, LOW);
+    }
+    newOut(a, LOW);
+  }
+
+  //newOut(0,HIGH);
+  //newOut(8,HIGH);
+
+}
+
+void newOut(int pin, int std){
 
   int i = 0;
   for(int p = 0; p <= 7; p++){
@@ -42,13 +88,13 @@ void Output(int pin,int std){
     }    
     i++;
   }
-  Output_Byte(a, b);
+  outByte(a, b);
   
 }
 
-void Output_Byte(int a, int b){
+void outByte(int a, int b){
     digitalWrite(STR, LOW);
-    shiftOut(DATA, CLK, MSBFIRST, b);
-    shiftOut(DATA, CLK, MSBFIRST, a);  
+    shiftOut(DATA_OUT, CLK, MSBFIRST, b);
+    shiftOut(DATA_OUT, CLK, MSBFIRST, a);  
     digitalWrite(STR, HIGH);
 }
